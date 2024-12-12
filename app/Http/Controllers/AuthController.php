56a3ca19d6e7ec8df868a0592438a8400dcfaec3
araujoleonardo <araujoleonardo.ti@gmail.com>
2024-12-12 10:21:10 -0400

    Backend:Add auth with laravel sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthFormRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(AuthFormRequest $request): JsonResponse
+    {
+        try {
+            $user = new User;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->string('password'));
+            $user->save();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ],
+                'token' => $token,
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()]);
+        }
+
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6',
+        ],
+        [
+            'email.required' => 'E-mail é obrigatório!',
+            'email.string' => 'E-mail inválido!',
+            'email.email' => 'E-mail não tem o formato desejado!',
+
+            'password.required' => 'Senha é obrigatória!',
+            'password.string' => 'Formato de senha inválido!',
+            'password.min' => 'Senha deve ter pelo menos 6 caracteres!',
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['password'], $user->password)){
+            return response()->json([
+                'message' => 'Usuário ou senha incorretos.',
+            ], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
+            'token' => $token,
+        ]);
+    }
+
+    public function auth(): JsonResponse
+    {
+        if (auth('sanctum')->check()) {
+            return response()->json([
+                'message' => 'Usuário autenticado!',
+                'user' => auth('sanctum')->user(),
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Usuário não autenticado!',
+            ], 401);
+        }
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $user = User::findOrFail($request->user_id);
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Usuário desconectado!',
+        ]);
+    }
+}
---
+++ app/Http/Requests/AuthFormRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:3',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nome é obrigatório!',
+            'name.min' => 'Nome deve ter pelo menos 3 caracteres!',
+            'name.string' => 'Nome precisa ser texto!',
+
+            'email.required' => 'E-mail é obrigatório!',
+            'email.string' => 'E-mail inválido!',
+            'email.email' => 'E-mail não tem o formato desejado!',
+            'email.unique' => 'E-mail já cadastrado!',
+
+            'password.required' => 'Senha é obrigatória!',
+            'password.string' => 'Formato de senha inválido!',
+            'password.min' => 'Senha deve ter pelo menos 6 caracteres!',
+            'password.confirmed' => 'Confirme a senha!',
+        ];
+    }
+}
